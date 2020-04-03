7bc43b02b154c9d43b2a8f4ce9df1ca877a2d363
stybl <sty@stybl.net>
2020-04-03 19:45:31 +0300

    Added database framework

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+// Import app from its own file
 const app = require('./app');
+
+// SET the port here
 const port = process.env.PORT || '8000';
 
-app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
+// Database dependencies
+const bodyParser = require('body-parser');
+const lowdb = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+
+// Set JSON encoding
+app.use(bodyParser.json());
+
+// Initialize database, pass it to request context, then launch app
+const adapter = new FileAsync('.data/db.json');
+
+lowdb(adapter)
+  .then(db => {
+    app.set('db', db);
+    return db.defaults().write();
+})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Listening to requests on http://localhost:${port}`);
+    });
 });
