370c61cbd9e90e1e391b238347e305d34d0ee6d1
stybl <sty@stybl.net>
2020-04-13 20:26:07 +0300

    Documented validation models

--- models/author.js
+++ models/author.js
@@ -1,28 +1,32 @@
 const { checkSchema } = require('express-validator');
 
+// Validation model for Author objects
 const schema = {
     id: {
-        in: ['param', 'body'],
-        isEmail: true,
-        escape: true,
+        in: ['param', 'body'], // Id is expected to be a parameter, or in the request body
+        isEmail: true, // It must be an email
+        escape: true, // It will be escaped, trimmed, and normalized by validator.js
         trim: true,
         normalizeEmail: true
     },
 
     name: {
-        in: ['body'],
+        in: ['body'], // Name is expected to be in the request body
         matches: {
             options: /^[a-z ]+$/i, // Only characters allowed are a-z (case insensitive) and spaces
             errorMessage: 'Invalid characters in string'
         },
-        notEmpty: true,
-        escape: true,
+        notEmpty: true, // It must not be empty
+        escape: true, // It will be escaped and trimmed
         trim: true
     }
 };
 
+// Validation for individual request types
 module.exports = {
-    schema: schema,
+    schema: schema, // The schema itself is included
+
+    // GETs must have an Id
     GET: checkSchema({
         id: {
             exists: true,
@@ -30,6 +34,7 @@ module.exports = {
         }
     }),
 
+    // POSTS must have an Id and Name
     POST: checkSchema({
         id: {
             exists: true,
@@ -41,6 +46,7 @@ module.exports = {
         }
     }),
 
+    // PUTs must have an Id, and optionally can have a name
     PUT: checkSchema({
         id: {
             exists: true,
--- models/image.js
+++ models/image.js
@@ -1,8 +1,12 @@
-const { schema: author } = require('./author');
-const { schema: program } = require('./program');
 const { checkSchema } = require('express-validator');
 
+const { schema: author } = require('./author');
+const { schema: program } = require('./program');
+
+// Validation model for requests made to the imageRouter
 module.exports = {
+
+    // POSTS must have a sessionId and a program Id
     POST: checkSchema({
         sessionId: {
             exists: true,
